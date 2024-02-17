5e2e990c4e73ab75c8e32131498aa4ec943c6b93
athulraj10 <athulraj165@gmail.com>
2024-02-17 14:06:24 +0400

    deleted  category file delted

--- backend/src/adapters/data-access/repositories/category/category.ts
+++
@@ -1,9 +0,0 @@
-import Category from "../../models/categoryModel";
-export default {
-    addCategory: async (details: Object): Promise<boolean> => {
-        const category = new Category({ ...details });
-        const status = await category.save();
-        console.log(status)
-        return !!status;
-    }
-}
