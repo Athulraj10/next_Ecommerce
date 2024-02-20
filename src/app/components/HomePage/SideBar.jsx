70dacdc62df48b1f1dc92f5ef72d3f7d70855e3c
athulraj10 <athulraj165@gmail.com>
2024-02-20 18:07:32 +0400

    sidebar still pending

--- src/app/components/HomePage/SideBar.jsx
+++ src/app/components/HomePage/SideBar.jsx
@@ -12,7 +12,7 @@ const SideBar = () => {
     <div>
       <button
         type="button"
-        className="fixed top-4 left-4 z-50 p-2 text-gray-500 rounded-lg sm:hidden bg-white shadow-md"
+        className="fixed top-4 left-4 z-50 p-2 text-gray-100 rounded-lg sm:hidden bg-white shadow-md"
         onClick={toggleSidebar}
       >
         <span className="sr-only">Open sidebar</span>
