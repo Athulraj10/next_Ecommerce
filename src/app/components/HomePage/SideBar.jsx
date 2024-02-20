04e63360b1b839541b22ac01b940eac1ca4f2495
athulraj10 <athulraj165@gmail.com>
2024-02-20 18:05:54 +0400

    sidebar button not visible

--- src/app/components/HomePage/SideBar.jsx
+++ src/app/components/HomePage/SideBar.jsx
@@ -9,17 +9,17 @@ const SideBar = () => {
   };
 
   return (
-    <>
+    <div>
       <button
         type="button"
-        className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        className="fixed top-4 left-4 z-50 p-2 text-gray-500 rounded-lg sm:hidden bg-white shadow-md"
         onClick={toggleSidebar}
       >
         <span className="sr-only">Open sidebar</span>
         <svg
-          className="w-6 h-6 text-gray-500 dark:text-gray-400"
+          className="w-6 h-6 text-gray-500"
           aria-hidden="true"
-          fill="currentColor"
+          fill="none"
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
         >
@@ -34,11 +34,12 @@ const SideBar = () => {
       <aside
         id="sidebar-multi-level-sidebar"
         className={`fixed top-0 left-0 z-40 w-100 h-screen transition-transform ${
-          isSidebarOpen ? "" : "-translate-x-full"
-        } sm:translate-x-0`}
-        aria-label="Sidebar"
-      >
-        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+            isSidebarOpen ? "" : "-translate-x-full"
+          } sm:translate-x-0`}
+          aria-label="Sidebar"
+        >
+  
+        <div className="h-full px-3 overflow-y-auto bg-gray-50 dark:bg-gray-800 py-[50px]">
           <ul className="space-y-2 font-medium">
            
            
@@ -432,7 +433,7 @@ const SideBar = () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
