914e257618f4967e0ee2516dc8faab38317b3dac
athulraj10 <athulraj165@gmail.com>
2024-02-20 14:46:23 +0400

    layout added

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LoginForm from '@/app/Components/loginForm/LoginForm'
+
+const Login = () => {
+  return (
+    <>
+        Login header
+        <LoginForm/>
+    </>
+  )
+}
+
+export default Login
