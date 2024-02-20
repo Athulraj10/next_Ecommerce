ceeb4bc4b43d2324d818134b3e4d1d7faf55cf76
athulraj10 <athulraj165@gmail.com>
2024-02-20 16:14:37 +0400

    added form validation

--- src/app/components/registerForm/Form.jsx
+++ src/app/components/registerForm/Form.jsx
@@ -1,154 +1,149 @@
-import { useState } from "react";
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
-const Form = () => {
-  const [formData, setFormData] = useState({
+const RegisterForm = () => {
+  const initialValues = {
     email: "",
-    gender: "",
+    gender: "male",
     password: "",
+    phone:"",
     confirmPassword: "",
-    acceptedTerms: false,
-  });
-  const [errors, setErrors] = useState({});
+    acceptedTerms: false
+  };
 
-  const handleChange = (e) => {
-    const { name, value, type, checked } = e.target;
-    let newErrors = { ...errors };
+  const onSubmit = (values) => {
+    console.log(values);
+  };
 
-    // Word validation
-    if (type === "text" && !/^[a-zA-Z\s]+$/.test(value)) {
-      newErrors[name] = "Please enter only letters and spaces.";
-    } else {
-      delete newErrors[name];
+  const validate = (values) => {
+    const errors = {};
+
+    if (!values.email) {
+      errors.email = "Required";
+    } else if (!/^\S+@\S+\.\S+$/.test(values.email)) {
+      errors.email = "Invalid email address";
     }
 
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: type === "checkbox" ? checked : value,
-    }));
-    setErrors(newErrors);
+    if (!values.password) {
+      errors.password = "Required";
+    } else if (values.password.length < 8) {
+      errors.password = "Password must be at least 8 characters long";
+    }
+
+    if (!values.confirmPassword) {
+      errors.confirmPassword = "Required";
+    } else if (values.confirmPassword !== values.password) {
+      errors.confirmPassword = "Passwords do not match";
+    }
+
+    if (!values.phone) {
+        errors.phone = "Required";
+      } else if (!/^\d{10}$/.test(values.phone)) {
+        errors.phone = "Invalid phone number. Please enter a 10-digit phone number without any spaces or special characters.";
+      }
+    
+      if (!values.acceptedTerms) {
+        errors.acceptedTerms = "Required";
+      } else if (!(values.acceptedTerms)) {
+        errors.acceptedTerms = "Invalid phone number. Please enter a 10-digit phone number without any spaces or special characters.";
+      }
+
+    return errors;
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
-  };
   return (
-    <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
-      <div>
-        <label
-          htmlFor="email"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Your email
-        </label>
-        <input
-          //   type="email"
-          name="email"
-          id="email"
-          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="name@gmail.com"
-          //   required=""
-          onChange={handleChange}
-        />
-      </div>
-      {errors.email && <p className="text-red-500">{errors.email}</p>}
-
-      <div>
-        <label
-          htmlFor="gender"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Gender
-        </label>
-        <select
-          id="gender"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-auto p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          required
-          defaultValue="male" 
-          onChange={handleChange} 
-        >
-          <option disabled>Choose one</option>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-          <option value="other">Other</option>
-        </select>
-      </div>
-
-      <div>
-        <label
-          htmlFor="password"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Password
-        </label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="••••••••"
-          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          required=""
-        />
-      </div>
-      <div>
-        <label
-          htmlFor="confirm-password"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Confirm password
-        </label>
-        <input
-          type="confirm-password"
-          name="confirm-password"
-          id="confirm-password"
-          placeholder="••••••••"
-          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          required=""
-        />
-      </div>
-      <div className="flex items-start">
-        <div className="flex items-center h-5">
-          <input
-            id="terms"
-            aria-describedby="terms"
-            type="checkbox"
-            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-            required=""
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validate={validate}
+    >
+      <Form className="space-y-4 md:space-y-6">
+        <div>
+          <label htmlFor="email">Your email</label>
+          <Field
+            type="email"
+            name="email"
+            id="email"
+            placeholder="name@gmail.com"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
+          <ErrorMessage name="email" component="div" className="text-red-500" />
         </div>
-        <div className="ml-3 text-sm">
-          <label
-            htmlFor="terms"
-            className="font-light text-gray-500 dark:text-gray-300"
-          >
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <Field
+            type="password"
+            name="password"
+            id="password"
+            placeholder="••••••••"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+          <ErrorMessage name="password" component="div" className="text-red-500" />
+        </div>
+
+        <div>
+          <label htmlFor="confirmPassword">Confirm password</label>
+          <Field
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+            placeholder="••••••••"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+          <ErrorMessage name="confirmPassword" component="div" className="text-red-500" />
+        </div>
+
+    <div>
+        <label htmlFor="number">Enter your number</label>
+        <Field
+        type='number'
+        name='phone'
+        id='mobile'
+        placeholder='+91 9999999999'
+        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+        />
+         <ErrorMessage name="phone" component="div" className="text-red-500" />
+    </div>
+      
+        <div className="flex items-start">
+          <Field
+            type="checkbox"
+            id="acceptedTerms"
+            name="acceptedTerms"
+            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+          />
+          <label htmlFor="acceptedTerms" className="ml-3 text-sm">
             I accept the
             <a
-              className="font-medium text-primary-600 hover:underline dark:text-primary-500"
               href="#"
-              required
+              className="font-medium text-primary-600 hover:underline dark:text-primary-500"
             >
               Terms and Conditions
             </a>
+          <ErrorMessage name='acceptedTerms' component='div' className="text-red-500"  />
           </label>
         </div>
-      </div>
-      <button
-        type="submit"
-        className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-      >
-        Create an account
-      </button>
-      <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-        Already have an account?
-        <a
-          href="#"
-          className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+
+        <button
+          type="submit"
+          className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
         >
-          Login here
-        </a>
-      </p>
-    </form>
+          Create an account
+        </button>
+
+        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+          Already have an account?
+          <a
+            href="#"
+            className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+          >
+            Login here
+          </a>
+        </p>
+      </Form>
+    </Formik>
   );
 };
 
-export default Form;
+export default RegisterForm;
