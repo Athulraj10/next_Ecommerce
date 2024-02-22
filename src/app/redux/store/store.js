80714a6730dc296ab53f6d5dfe8a47b9780dac11
athulraj10 <athulraj165@gmail.com>
2024-02-22 12:03:36 +0400

    redux cread

--- src/app/redux/store/store.js
+++ src/app/redux/store/store.js
@@ -1,7 +1,7 @@
 // import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from "redux-saga";
-import rootReducer from "./../reducer";
-import rootSaga from "./../saga";
+import rootReducer from "../reducer";
+import rootSaga from "../saga";
 import { configureStore } from "@reduxjs/toolkit";
 
 const sagaMiddleware = createSagaMiddleware();
